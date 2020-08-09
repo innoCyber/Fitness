0ef841ea0b9ebf28b6f77f2691ad08201cd5d7fd
innoCyber <innocentileka@gmail.com>
2020-08-09 20:29:13 +0100

    Setup location access permission

--- app/src/main/java/com/innocyber/fitnesstracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/innocyber/fitnesstracker/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.innocyber.fitnesstracker.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,19 +9,69 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.innocyber.fitnesstracker.R
 import com.innocyber.fitnesstracker.ui.viewmodels.MainViewModel
+import com.innocyber.fitnesstracker.util.Constants.LOCATION_PERMISSION_REQUEST_CODE
+import com.innocyber.fitnesstracker.util.PermissionUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment: Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermission()
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
+
+    private fun requestPermission() {
+        if (PermissionUtil.hasLocationPermission(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "Location permission is required to use this app",
+                LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "Location permission is required to use this app",
+                LOCATION_PERMISSION_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
--- app/src/main/java/com/innocyber/fitnesstracker/util/Constants.kt
+++ app/src/main/java/com/innocyber/fitnesstracker/util/Constants.kt
@@ -3,4 +3,5 @@ package com.innocyber.fitnesstracker.util
 object Constants {
 
     const val DATABASE_NAME = "run_db"
+    const val LOCATION_PERMISSION_REQUEST_CODE = 100
 }
